3e64c262fd3964e9829ff56446189cd82e1bfcf4
Robert Petzold <RobertP@ppedv.de>
2019-01-17 10:40:56 +0100

    Wetter Properties

--- WetterApp/CityWetter.xaml.cs
+++ WetterApp/CityWetter.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -18,7 +20,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace WetterApp
 {
-    public sealed partial class CityWetter : UserControl
+    public sealed partial class CityWetter : UserControl, INotifyPropertyChanged
     {
 
         public string Stadt
@@ -29,13 +31,13 @@ namespace WetterApp
 
         // Using a DependencyProperty as the backing store for Stadt.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty StadtProperty =
-            DependencyProperty.Register("Stadt", typeof(string), typeof(CityWetter), new PropertyMetadata(null,StadtChanged));
+            DependencyProperty.Register("Stadt", typeof(string), typeof(CityWetter), new PropertyMetadata(null, StadtChanged));
 
         private static void StadtChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if(d is CityWetter cityWetter)
+            if (d is CityWetter cityWetter)
             {
-                if(!string.IsNullOrWhiteSpace(e.NewValue.ToString()))
+                if (!string.IsNullOrWhiteSpace(e.NewValue.ToString()))
                 {
                     cityWetter.LadeWetter();
                 }
@@ -44,13 +46,50 @@ namespace WetterApp
 
         private string _error = string.Empty;
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string Error
         {
             get { return _error; }
-            set { _error = value; }
+            set
+            {
+                _error = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Error)));
+            }
         }
 
+        private int _temp;
+        public int Temperatur
+        {
+            get { return _temp; }
+            set
+            {
+                _temp = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Temperatur)));
+            }
+        }
 
+        private string _beschreibung;
+        public string Beschreibung
+        {
+            get { return _beschreibung; }
+            set
+            {
+                _beschreibung = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Beschreibung)));
+            }
+        }
+
+        private string  _iconUrl;
+        public string IconUrl
+        {
+            get { return _iconUrl; }
+            set
+            {
+                _iconUrl = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IconUrl)));
+            }
+        }
 
         public async void LadeWetter()
         {
@@ -58,7 +97,10 @@ namespace WetterApp
             {
                 HttpClient client = new HttpClient();
                 string jsonResult = await client.GetStringAsync($"https://api.openweathermap.org/data/2.5/weather?q={Stadt}&units=metric&appid=84d84c7b399d88e7f4e4688facc2498e");
-
+                var apiresult = JsonConvert.DeserializeObject<WetterAPIResult>(jsonResult);
+                Temperatur = apiresult.main.temp_min;
+                IconUrl = apiresult.weather[0].IconUrl;
+                Beschreibung = apiresult.weather[0].description;
             }
             catch (Exception)
             {
